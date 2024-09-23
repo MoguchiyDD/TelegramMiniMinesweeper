93c7c1a8e9a4daec601f0bc36f754a17c351b860
MoguchiyDD <ket-ket-01@bk.ru>
2024-09-24 01:42:26 +0500

    fix: logic mines around

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const mineCount = 10;
 const gridSize = 9;
-let mines = Array(gridSize).fill(null).map(() => Array(gridSize).fill(false));
+const axys = [
+  [-1, 1], [0, 1], [1, 1],
+  [1, -1], [1, 0], [1,-1],
+  [0,-1], [-1, -1], [-1,0]
+];
+let cells = Array(gridSize).fill(null).map(() => Array(gridSize).fill(false));
 
 const fillCounts = () => {
   document.getElementById("countMines").innerHTML += mineCount;
@@ -19,8 +24,8 @@ const initGame = () => {
     do {
       x = Math.floor(Math.random() * gridSize);
       y = Math.floor(Math.random() * gridSize);
-    } while (mines[x][y]);
-    mines[x][y] = true;
+    } while (cells[x][y]);
+    cells[x][y] = true;
   }
 
   // cells
@@ -35,14 +40,40 @@ const initGame = () => {
 }
 
 const revealCell = (e, x, y) => {
-  console.log(mines);
-  if (mines[x][y]) {
+  console.log(cells);
+  if (cells[x][y]) {
     alert("Game Over! You hit a mine.");
     initGame();  // reboot
   } else {
     console.log(e);
-    e.style.backgroundColor = "var(--tg-theme-accent-text-color)";
+    e.target.style.backgroundColor = "var(--tg-theme-accent-text-color)";
+
+    const minesAround = findMinesAround(x, y);
+    if (minesAround >= 1) {
+      e.target.innerHTML = minesAround;
+    }
   }
+}
+
+const findMinesAround = (xstart, ystart) => {
+  const x = xstart;
+  const y = ystart;
+
+  let countMinesAround = 0;
+  let dotLoop = 0;
+
+  while(dotLoop < axys.length) {
+    try {
+      if (cells[x + axys[dotLoop][0]][y + dotLoop[1]]) {
+        ++countMinesAround;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    dotLoop++;
+  }
+
+  return countMinesAround;
 }
 
 fillCounts();
