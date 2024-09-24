a2b7527b00e39a6f2da3e67514d35aa92898c83b
MoguchiyDD <ket-ket-01@bk.ru>
2024-09-25 02:35:28 +0500

    fix: style

--- js/gameover.js
+++ js/gameover.js
@@ -1,28 +1,3 @@
-// const gameOver = (res) => {
-//   const _cells = arrayCellsButton();
-//   cells.forEach((cell, i) => {
-//     cell.forEach((btn, j) => {
-//       if (btn === true) {
-//         if (_cells[i][j].style != colorSubtitleText) {
-//           _cells[i][j].style.backgroundColor = colorDestructiveText;
-//           _cells[i][j].style.borderColor = colorDestructiveText;
-//         }
-//       }
-//     })
-//     notificationText.innerHTML = `Game Over! You ${res}. Play again?`;
-//     notification.style.display = "flex";
-//   })
-// }
-
-// notificationBtnYes.addEventListener("click", () => {
-
-// })
-
-// notificationBtnNo.addEventListener("click", () => {
-  
-// })
-
-
 function gameOver(res) {
   tg.showConfirm(`Game Over! You ${res}. Play again?`, (confirmed) => {
     if (confirmed) {
