3d86d9fed75a543ef87e3bb169cfefbbbefd9777
MoguchiyDD <ket-ket-01@bk.ru>
2024-09-25 03:00:04 +0500

    fix: style

--- js/colors.js
+++ js/colors.js
@@ -1,4 +1,4 @@
-const colorButton = "rgba(var(--tg-theme-button-color), 0.9)";
+const colorButton = "var(--tg-theme-button-color)";
 const colorAccentText = "var(--tg-theme-accent-text-color)";
 const colorSubtitleText = "var(--tg-theme-subtitle-text-color)";
 const colorDestructiveText = "var(--tg-theme-destructive-text-color)";
