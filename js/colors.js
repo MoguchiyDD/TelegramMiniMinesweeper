804c1fd692a2e2ea1a3d92941218f6b838133124
MoguchiyDD <ket-ket-01@bk.ru>
2024-09-25 15:56:12 +0500

    fix: style

--- js/colors.js
+++ js/colors.js
@@ -1,6 +1,5 @@
-// const colorButton = "rgb(51, 65, 85)";  // "var(--tg-theme-button-color)";
+
 const colorButton = "rgb(30, 41, 59)";  // "var(--tg-theme-button-color)";
-// const colorAccentText = "#1E293B";  // "var(--tg-theme-accent-text-color)";
 const colorAccentText = "#334155";  // "var(--tg-theme-accent-text-color)";
 const colorSubtitleText = "var(--tg-theme-subtitle-text-color)";
 const colorDestructiveText = "#F1F5F9";  // "var(--tg-theme-destructive-text-color)";
